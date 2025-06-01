ce81437b6b6ee424f0763fbac1cd2848c74d369f
Jatin2662 <creativejatin111@gmail.com>
2025-06-01 20:48:28 +0530

    custom email page built

--- frontend/src/pages/admin/AdminCustomEmail.js
+++ frontend/src/pages/admin/AdminCustomEmail.js
@@ -1,19 +1,74 @@
 
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { hideNav } from "../../redux/slice/navSlice";
+import '../../styles/AdminCustomEmail.css';
 
 function AdminCustomEmail(){
 
     const dispatch = useDispatch();
+
+    const [mailData, setMailData] = useState({
+        subject: '',
+        body: '',
+        end: ''
+    })
+
+    const handleFormChange = (e) =>{
+
+        setMailData({ ...mailData, [e.target.name] : e.target.value })
+    }
 
     useEffect(()=>{
         dispatch(hideNav('Custom Email'))
     })
 
     return(
-        <h1>Set custom email message one deafult in backend and other from req.body</h1>
+        <main className="admin-mail">
+            <div className="admin-header">
+                <h1>Set Email content</h1>
+            </div>
+            <form>
+                <div className="input-fields">
+                    <label htmlFor="subject">Subject</label>
+                    <input
+                    type="text"
+                    placeholder="Subject"
+                    name="subject"
+                    id="subject"
+                    value={mailData.subject}
+                    onChange={handleFormChange}
+                    required
+                    />
+                </div>
+                <div className="input-fields">
+                    <label htmlFor="body">Body</label>
+                    <textarea
+                    type="text"
+                    placeholder="Body"
+                    name="body"
+                    id="body"
+                    value={mailData.body}
+                    onChange={handleFormChange}
+                    required
+                    rows={5}
+                    />
+                </div>
+                <div className="input-fields">
+                    <label htmlFor="end">End</label>
+                    <input
+                    type="text"
+                    placeholder="End"
+                    name="end"
+                    id="end"
+                    value={mailData.end}
+                    onChange={handleFormChange}
+                    required
+                    />
+                </div>
+            </form>
+        </main>
     );
 }
 
