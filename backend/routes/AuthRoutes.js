c5c49aa559a1f9192aa1f2d44e6ab7ae9349a11a
Jatin2662 <creativejatin111@gmail.com>
2025-06-16 19:10:57 +0530

    addition of reset password paths, adding middlewares to get different links

--- backend/routes/AuthRoutes.js
+++ backend/routes/AuthRoutes.js
@@ -1,14 +1,25 @@
 
 
 const express = require('express');
-const { signup, login, verifyEmail, EmailVerification } = require('../controllers/UserAuthenticationController');
-const { signupValidation, loginValidation } = require('../middlewares/AuthValidation');
+const { signup, login, verifyEmail, EmailVerification, getPasswordChangeRequest, resetPassword } = require('../controllers/UserAuthenticationController');
+const { signupValidation, loginValidation, emailVerificationValidation, resetPasswordValidation } = require('../middlewares/AuthValidation');
 const router = express.Router();
 
 
 router.post('/signup', signupValidation, signup);
 router.post('/login', loginValidation, login);
+
 router.get('/verify/:token', verifyEmail)
-router.post('/verify', EmailVerification )
+router.post('/verify', emailVerificationValidation, (req, res, next) => {
+    req.body.linkName = 'verify';
+    next();
+}, EmailVerification )
+
+router.post('/reset-password', emailVerificationValidation, (req, res, next) => {
+    req.body.linkName = 'reset-password';
+    next();
+}, EmailVerification )
+router.get('/reset-password/:token', getPasswordChangeRequest)
+router.post('/reset-password/:token', resetPasswordValidation, resetPassword)
 
 module.exports = router;
